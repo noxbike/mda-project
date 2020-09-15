257ef36362081211ad366e5a952a25d66daf38a9
noxbike <noxbike@gmail.com>
2020-09-15 16:17:41 +0400

    caroussel ajouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './component/Navbar';
+import Caroussel from './component/Caroussel';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Caroussel/>
     </div>
   );
 }
